0d31e7b90a2f9b0865650848afaa12722dae9ae1
shrimp <liu.shrimp@gmail.com>
2018-04-13 12:21:40 +0800

    propose action

--- more.voting/more.voting.cpp
+++ more.voting/more.voting.cpp
@@ -7,10 +7,16 @@ using namespace std;
 typedef uint8_t vcount;
 
 struct proposal {
+    account_name proposer;
     name pname;
     string description;
     vcount votes = 0;
-    EOSLIB_SERIALIZE( proposal, (pname)(description)(votes))
+
+    friend bool operator == ( const proposal& a, const proposal& b ) {
+        return a.pname == b.pname;
+    }
+
+    EOSLIB_SERIALIZE( proposal, (proposer)(pname)(description)(votes))
 };
 
 class voting : public contract {
@@ -21,7 +27,7 @@ public:
 
     // @abi action
     void create( account_name creator, name vname, time expiration, vector<proposal> proposals = {} ) {
-        require_auth(creator);
+        require_auth( creator );
 
         eosio_assert( expiration > now(), "expiration cannot be earlier than current" );
 
@@ -33,6 +39,23 @@ public:
             row.expiration = expiration;
             row.proposals = move(proposals);
         });
+    }
+
+    // @abi action
+    void propose( account_name proposer, account_name creator, name vname, proposal proposal ) {
+        require_auth( proposer );
+
+        vrecords record_table(_self, creator);
+        auto record_itr = record_table.find( vname );
+        eosio_assert( record_itr != record_table.end(), "voting not found" );
+
+        auto prop_itr = std::find( record_itr->proposals.begin(), record_itr->proposals.end(), proposal );
+        eosio_assert( prop_itr == record_itr->proposals.end(), "proposal with the same name exists" );
+
+        record_table.modify( record_itr, proposer, [&]( auto& row ) {
+            row.proposals.push_back( proposal );
+        });
+
     }
 
     // @abi action
@@ -59,4 +82,4 @@ private:
     typedef multi_index<N(vrecord), vrecord> vrecords;
 };
 
-EOSIO_ABI( voting, (create)(cancel) )
+EOSIO_ABI( voting, (create)(propose)(cancel) )
